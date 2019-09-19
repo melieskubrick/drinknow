dfc496cc89f75740ae62bacce7f2472d0a0266a6
Melies Kubrick <macbook@MacBook-Pro-de-Melies-Kubrick.local>
2019-09-19 15:08:03 -0300

    Conexão com a API do thecocktaildb

---
+++ DrinkNow/Categorias/Itens/ItensCategoriaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItensCategoriaTableViewCell.swift
+//  DrinkNow
+//
+//  Created by Melies Kubrick on 19/09/19.
+//  Copyright © 2019 Melies Kubrick. All rights reserved.
+//
+
+import UIKit
+
+class ItensCategoriaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
